6c5cb6fc0a5d74ddda4404f22fa09951840aebbc
SavionW-Works <72318904+SavionW-Works@users.noreply.github.com>
2024-04-17 20:00:16 -0400

    Added better API calls and Loading Icon

--- youtube_clone/src/components/AppContentArea.jsx
+++ youtube_clone/src/components/AppContentArea.jsx
@@ -1,10 +1,36 @@
 import Box from "@mui/material/Box";
-import React, { useState } from "react";
 import SideList from "@components/SideList";
 import TabList from "@components/TabList";
 import CardList from "@components/CardList";
 import { appContentWrapper, flexColumnGrow } from "@styles/styles";
+import React, { useState, useEffect } from "react";
+import { fetchFromApi } from "../utils/api";
+//import { fetchFromApi } from "@utils/api";
+import Loader from "@components/Loader";
 const AppContentArea = ({ isOpen }) => {
+  const [youtubeData, setYoutubeData] = useState([]);
+  const [searchText, setSearchText] = useState("All");
+  const [loading, setLoading] = useState(false);
+
+  useEffect(() => {
+    try {
+      fetchFromApi(`/search?part=snippet&q=${searchText}`).then((response) => {
+        setYoutubeData(response.data.items);
+        setLoading(false);
+      });
+    } catch (e) {
+      setLoading(false);
+    }
+  }, [searchText]);
+
+  const onTabChange = (searchValue) => {
+    setSearchText(searchValue);
+    setLoading(!loading);
+  };
+
+  if (!youtubeData.length) {
+    return;
+  }
   const sideBarWidth = isOpen ? "70px" : "250px";
   return (
     <Box component="main" sx={appContentWrapper}>
@@ -26,8 +52,23 @@ const AppContentArea = ({ isOpen }) => {
             width: `calc(100vw - ${sideBarWidth})`,
           }}
         >
-          <TabList />
-          <CardList />
+          <TabList onTabChange={onTabChange} />
+        </Box>
+
+        <Box
+          component="div"
+          sx={{
+            display: "block",
+            p: 2,
+            minHeight: "100px",
+            mb: 4,
+            overflow: "hidden",
+            overflowY: "auto",
+            width: `calc(100vw - ${sideBarWidth})`,
+          }}
+        >
+          <CardList items={youtubeData} />
+          <Loader open={loading} />
         </Box>
       </Box>
     </Box>
--- youtube_clone/src/utils/api.js
+++ youtube_clone/src/utils/api.js
@@ -3,21 +3,24 @@ import axios from "axios";
 export const BASE_URL = "https://youtube-v31.p.rapidapi.com";
 
 const options = {
-  method: "GET",
-  url: BASE_URL,
   params: {
-    maxResults: 50,
+    maxResults: "50",
   },
   headers: {
-    "X-RapidAPI-Key": "a9ad17a5a9msh1a9a2e041bc7a99p12b6ddjsnc831cb920e37",
+    "X-RapidAPI-Key": process.env.REACT_APP_RAPID_API_KEY,
     "X-RapidAPI-Host": "youtube-v31.p.rapidapi.com",
   },
 };
 
-const fetchFromApi = async (url) => {
-  const { data } = await axios.get(`${BASE_URL}/${url}`, options);
-  console.log(";;;;", data);
-  return data;
-};
+export const fetchFromApi = (endpoint, searchQuery) => {
+  const apiOptions = {
+    ...options,
+    url: `${BASE_URL}${endpoint}`,
+    params: {
+      ...options.params,
+      q: searchQuery,
+    },
+  };
 
-export default fetchFromApi;
+  return axios.request(apiOptions);
+};
